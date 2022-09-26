52c572c732d9f95f5129be76aca79c8b5025b0ef
Calvin Wilkinson <kinsondigital@gmail.com>
2022-09-26 07:51:50 +0100

    Improved dotnet tool install script

--- CleanReinstallTool.ps1
+++ CleanReinstallTool.ps1
@@ -1,12 +1,25 @@
+$baseSoftwareDevDirPath = "K:\SOFTWARE DEVELOPMENT";
+$personalSoftware = "PERSONAL";
+$projectName = "CICD";
+$scriptFileName = "CreateToolPackage.ps1";
+$packageName = "KinsonDigital.CICD";
+$solutionDirPath = "$baseSoftwareDevDirPath/$personalSoftware/$projectName";
+$packageSourcePath = "$solutionDirPath/$projectName";
 
-$basePath = "../CICDTest";
+# Path to other project script to build the package
+$fullScriptFilePath = "$solutionDirPath/$scriptFileName";
+
+# Execute the other script to create the package
+& $fullScriptFilePath;
+
+
+$basePath = "$PSScriptRoot";
+$configPath = "$basePath/.config";
 $nugetCacheFilePath = "C:/Users/$env:UserName/.nuget/packages/kinsondigital.cicd";
 
-$packagePaths = Get-ChildItem -Path "$basePath/**/KinsonDigital.CICD*.nupkg" -Recurse | ForEach-Object{$_.FullName};
-
 # Delete the dotnet tool manifest if it exists
-if (Test-Path -Path "$basepath/.config") {
-    Remove-Item -Path "$basepath/.config" -Force -Recurse -Confirm:$false;
+if (Test-Path -Path $configPath) {
+    Remove-Item -Path $configPath -Force -Recurse -Confirm:$false;
     Write-Host "✅DotNet tool manifest deleted.";
 }
 
@@ -20,6 +33,9 @@ if (Test-Path -Path $nugetCacheFilePath) {
 dotnet new tool-manifest
 
 # Installed the dotnet tool locally
-$packagePaths = Get-ChildItem -Path "$basePath/**/KinsonDigital.CICD*.nupkg" -Recurse | ForEach-Object{$_.Directory};
+$packagePath = Get-ChildItem -Path "$solutionDirPath/**/$packageName*.nupkg" -Recurse | ForEach-Object{$_.FullName};
 
-dotnet tool install "KinsonDigital.CICD" --add-source "$packagePaths" --version "1.0.0-preview.2"
+# Get the vesion
+$packageVersion = $packagePath.Replace(".nupkg", "").Split("$packageName.")[1];
+
+dotnet tool install "KinsonDigital.CICD" --add-source $packageSourcePath --version $packageVersion
