92309d4987a5377dccf7059d42eb66a15e87e9b7
Calvin Wilkinson <kinsondigital@gmail.com>
2022-09-29 23:19:22 +0100

    Improve dotnet tool install script

--- CleanReinstallTool.ps1
+++ CleanReinstallTool.ps1
@@ -1,41 +1,67 @@
+Clear-Host;
+
+
 $baseSoftwareDevDirPath = "K:\SOFTWARE DEVELOPMENT";
 $personalSoftware = "PERSONAL";
 $projectName = "CICD";
-$scriptFileName = "CreateToolPackage.ps1";
+$projectFileName = "$projectName.csproj";
 $packageName = "KinsonDigital.CICD";
 $solutionDirPath = "$baseSoftwareDevDirPath/$personalSoftware/$projectName";
-$packageSourcePath = "$solutionDirPath/$projectName";
+$projectDirPath = "$solutionDirPath/$projectName";
+$projectFilePath = "$projectDirPath/$projectFileName";
+$packageDirPath = "$PSScriptRoot/cicd-package";
 
-# Path to other project script to build the package
-$fullScriptFilePath = "$solutionDirPath/$scriptFileName";
-
-# Execute the other script to create the package
-& $fullScriptFilePath;
-
-
-$basePath = "$PSScriptRoot";
-$configPath = "$basePath/.config";
-$nugetCacheFilePath = "C:/Users/$env:UserName/.nuget/packages/kinsondigital.cicd";
-
-# Delete the dotnet tool manifest if it exists
-if (Test-Path -Path $configPath) {
-    Remove-Item -Path $configPath -Force -Recurse -Confirm:$false;
-    Write-Host "✅DotNet tool manifest deleted.";
-}
+$configPath = "$PSScriptRoot/.config";
 
 # Delete the cached nuget package tool
-if (Test-Path -Path $nugetCacheFilePath) {
+$nugetCacheFilePath = "C:/Users/$env:UserName/.nuget/packages/$packageName";
+if (Test-Path -Path $nugetCacheFilePath)
+{
     Remove-Item -Path $nugetCacheFilePath -Force -Recurse -Confirm:$false;
-    Write-Host "✅Globally cached dotnet tool nuget package deleted.";
+    Write-Host "✅Globally cached dotnet tool nuget package deleted`n";
 }
 
+# Delete the dotnet tool manifest if it exists
+if (Test-Path -Path $configPath)
+{
+    Remove-Item -Path $configPath -Force -Recurse -Confirm:$false;
+    Write-Host "✅DotNet tool manifest deleted`n";
+}
+
+# Delete all nuget pacakges
+if (Test-Path -Path $packageDirPath)
+{
+    # Delete all nuget packages
+    Remove-Item -Path "$packageDirPath" -Recurse -Confirm:$false;
+    Write-Host "✅All NuGet packages deleted`n";
+}
+
+# Build the CICD project
+dotnet build "$projectDirPath" -c "Debug";
+Write-Host "✅$projectName Project Built`n";
+
+# Create the nuget package
+dotnet pack $projectFilePath -c "Debug" -o $packageDirPath;
+Write-Host "✅$projectName NuGet package '$packageName' created`n";
+
 # Create the dotnet tool manifest
-dotnet new tool-manifest
+dotnet new tool-manifest --force
+Write-Host "✅DotNet Tool Manifest Restored`n";
 
-# Installed the dotnet tool locally
-$packagePath = Get-ChildItem -Path "$solutionDirPath/**/$packageName*.nupkg" -Recurse | ForEach-Object{$_.FullName};
+[string[]]$packages = Get-ChildItem -Path "$packageDirPath/*.nupkg" | ForEach-Object { $_.Name };
 
-# Get the vesion
-$packageVersion = $packagePath.Replace(".nupkg", "").Split("$packageName.")[1];
+# If there is more than 1 package, throw and error and fail
+if ($packages.Length -gt 1)
+{
+    Write-Host "❌There is more than 1 package to choose from.  Cannot choose a package to extract the version from.";
 
-dotnet tool install "KinsonDigital.CICD" --add-source $packageSourcePath --version $packageVersion
+    $packages | ForEach-Object { Write-Host "Package: $_" };
+
+    exit 1;
+}
+
+# Extract the the version from the nuget package name
+$packageVersion = $packages[0].Replace(".nupkg", "").Split("$packageName.")[1];
+
+dotnet tool install "$packageName" --add-source $packageDirPath --version "$packageVersion";
+Write-Host "✅$projectName DotNet Tool Installed";
